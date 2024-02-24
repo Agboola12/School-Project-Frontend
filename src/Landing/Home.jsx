c6042683696c84d016b6525ea68f859156582988
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-24 07:03:24 +0100

    wip on the landing page

--- src/Landing/Home.jsx
+++ src/Landing/Home.jsx
@@ -8,13 +8,13 @@ const Home = () => {
         <NavBar/>
 
         <header className={style.header}>
-        {/* <div class="text-center"  id="home">
+        <div class="text-center"  id="home">
           <h6 style="margin-bottom: 20%">sjdbmn</h6>
           <h2 class="text-light"  >Learning Today . . .  Leading Tomorrow</h2>
           <p class="text-light" >Knowledge is key to sucess</p>
           <button class="btn btn-success"> Take Admission</button>
           
-      </div> */}
+      </div>
     </header>
     </div>
   )
