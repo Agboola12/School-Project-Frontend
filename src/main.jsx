570f84ecd4a240749f2e7c1bfb7112894060989a
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-24 06:44:29 +0100

    wip on the landing page

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
-import './index.css'
-import Home from './Landing/Home.jsx'
+
+import Routing from './Routing.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
+    <BrowserRouter>
+        <Routing />
+    </BrowserRouter>
   </React.StrictMode>,
 )
