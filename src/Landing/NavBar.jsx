2bbc7bae9fe75ef3490a9df020769327ccf21d8f
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-24 06:49:58 +0100

    wip on the landing page

--- src/Landing/NavBar.jsx
+++ src/Landing/NavBar.jsx
@@ -2,7 +2,32 @@ import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div>
+      <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top ">
+
+<a href="" class="navbar-brand p-3">School Management</a>
+<button class="navbar-toggler" data-toggle="collapse" type="button" data-target="#sab">
+  <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="sab">    
+<ul class="navbar-nav list-line ml-auto">               
+<li class="nav-item active list-line-item mr-5 ">
+  <a href="home" class="nav-link active">Home</a>
+</li>
+
+<li class="nav-item dropdown list-line-item mr-5 " >
+  <a href="admin/first.php" class="nav-link">Admin</a>
+</li>
+<li class="nav-item dropdown  list-line-item mr-5" >
+<a href="teacher/first.php" class="nav-link" data-target="#login" data-toggle="modal">Teacher</a>
+</li>
+ <li class="nav-item dropdown list-line-item mr-5" >
+  <a href="student/first.php" class="nav-link ">Student</a>
+</li>
+</ul>
+</div>
+</nav>
+    </div>
   )
 }
 
