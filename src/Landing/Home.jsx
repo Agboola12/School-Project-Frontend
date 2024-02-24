e3bc5a52c6f6678b41e3d11dfaedec3721aade04
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-24 09:37:25 +0100

    wip on the landing page

--- src/Landing/Home.jsx
+++ src/Landing/Home.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import NavBar from './NavBar'
 import style from '../styles/Home.module.css'
 import colabo from '../assets/00.jpg' 
+import col from '../assets/aa.jpeg' 
 
 const Home = () => {
   return (
-    <div>
+    <div >
         <NavBar/>
 
         <header className={style.header}>
@@ -20,22 +21,65 @@ const Home = () => {
     <div className='container mt-5'>
       <div className='row mb-5'>
         <div className='col-lg-3 mr-3 shadow-lg bg-light p-5 mx-auto text-center'>
-          <img src={colabo} alt='kakd' className='w-75' style={{borderRadius: '50%'}} />
+          <img src={colabo} alt='kakd' className='w-75 h-50' style={{borderRadius: '50%'}} />
           <p className='fw-bold h4'>Collaborate</p>
           <p>Manage projects, organise tasks, and build spirit - all in a place</p>
         </div>
         <div className='col-lg-3 mr-3 shadow-lg bg-light p-5 mx-auto text-center'>
-          <img src={colabo} alt='kakd' className='w-75' style={{borderRadius: '50%'}} />
+          <img src={colabo} alt='kakd' className='w-75 h-50' style={{borderRadius: '50%'}} />
           <p className='fw-bold h4'>Collaborate</p>
           <p>Manage projects, organise tasks, and build spirit - all in a place</p>
         </div>
         <div className='col-lg-3 mr-3 shadow-lg bg-light p-5 mx-auto text-center'>
-          <img src={colabo} alt='kakd' className='w-75' style={{borderRadius: '50%'}} />
+          <img src={colabo} alt='kakd' className='w-75 h-50' style={{borderRadius: '50%'}} />
           <p className='fw-bold h4'>Collaborate</p>
           <p>Manage projects, organise tasks, and build spirit - all in a place</p>
         </div>
       </div>
     </div>
+
+    <div className='container mt-5 mb-5'>
+      <p className='h3 text-center fw-bold'>Lecturers</p>
+    <div className='row mt-5'>
+        <div className='col-lg-3 mr-5 d-flex'>
+          <img src={col} alt='tech' className=' w-50' style={{borderRadius:'50px'}}/>
+         <div className='ml-4'>
+          <small className='fw-bold h5'> Adebite </small><br/>
+          <small>Physics </small><br/>
+          <small>1,250,000 Students</small>
+        </div>
+        </div>
+        <div className='col-lg-3 mr-5 d-flex'>
+          <img src={col} alt='tech' className=' w-50' style={{borderRadius:'50px'}}/>
+         <div className='ml-4'>
+          <small className='fw-bold h5'> Adebite </small><br/>
+          <small>Physics </small><br/>
+          <small>1,250,000 Students</small>
+        </div>
+        </div>
+        <div className='col-lg-3 mr-5 d-flex'>
+          <img src={col} alt='tech' className=' w-50' style={{borderRadius:'50px'}}/>
+         <div className='ml-4'>
+          <small className='fw-bold h5'> Adebite </small><br/>
+          <small>Physics </small><br/>
+          <small>1,250,000 Students</small>
+        </div>
+        </div>
+    </div>
+        <a href='#' className='text-right'><p>More..</p></a>
+    </div>
+
+    <div class="container-fluid bg-dark text-light text-center p-4"  id="footer">
+    
+               <div class="mt-3">
+                <span class="fab fa-facebook m-1"></span>
+                <span class="fab fa-whatsapp m-1"></span>
+               <span class="fab fa-twitter m-1"></span>
+               <span class="fab fa-instagram m-1"></span>
+              </div> <br/>
+            
+            <p>© Copyright Agboola E.I All right reserved. Template by <a href="elijahagboola077">Elite</a>.</p>
+      </div>
     </div>
   )
 }
