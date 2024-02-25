35eea030f769065cb2a8a7dc077e03ff0ce0815b
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-25 21:47:03 +0100

    wip on the design

--- src/Landing/All-Lecturer.jsx
+++ src/Landing/All-Lecturer.jsx
@@ -5,53 +5,88 @@ import teach from '../images/lectu.jpg'
 
 
 const AllLecturer = () => {
-  return (
-    <div>
-        <LandNavBar/>
+    return (
+        <div>
+            <LandNavBar />
 
-        <div className='container mt-4'>
-                        <p className='fw-bold h3 mb-5'>Our Lecturers</p>
-                        <div className='row '>
-                            <div className='col-lg-3 mr-5 shadow border border-black p-3 w-25 mx-auto text-center  mb-5 rounded'>
-                                <div className='mb-3'>
-                                    <img src={teach} alt='iod' className='w-50' style={{ borderRadius: '20%' }} />
-                                </div>
-                                <div>
-                                    <p className='fw-bold'>Professor Adebite Hope</p>
-                                    <p>Physics and Mathematics</p>
-                                    <p>1,240 Student Tutored</p>
-                                    <button className='btn btn-outline-success'>View More</button>
-                                </div>
-                            </div>
-                            <div className='col-lg-3 mr-5 shadow border border-black p-3 w-25 mx-auto text-center  mb-5 rounded'>
-                                <div className='mb-3'>
-                                    <img src={teach} alt='iod' className='w-50' style={{ borderRadius: '20%' }} />
-                                </div>
-                                <div>
-                                    <p className='fw-bold'>Professor Adebite Hope</p>
-                                    <p>Physics and Mathematics</p>
-                                    <p>1,240 Student Tutored</p>
-                                    <button className='btn btn-outline-success'>View More</button>
-                                </div>
-
-                            </div>
-                            <div className='col-lg-3 mr-5 shadow border border-black p-3 w-25 mx-auto text-center  mb-5 rounded'>
-                                <div className='mb-3'>
-                                    <img src={teach} alt='iod' className='w-50' style={{ borderRadius: '20%' }} />
-                                </div>
-                                <div>
-                                    <p className='fw-bold'>Professor Adebite Hope</p>
-                                    <p>Physics and Mathematics</p>
-                                    <p>1,240 Student Tutored</p>
-                                    <button className='btn btn-outline-success'>View More</button>
-                                </div>
-                            </div>
-                            <Link to="/tutors" className='text-right nav-link'>View More</Link>
+            <div className='container mt-4'>
+                <p className='fw-bold h3 text-center mt-5 mb-5'>Our Lecturers</p>
+                <div className='row '>
+                    <div className='col-lg-3 mr-5 shadow border border-black p-3 w-25 mx-auto text-center  mb-5 rounded'>
+                        <div className='mb-3'>
+                            <img src={teach} alt='iod' className='w-50' style={{ borderRadius: '20%' }} />
+                        </div>
+                        <div>
+                            <p className='fw-bold'>Professor Adebite Hope</p>
+                            <p>Physics and Mathematics</p>
+                            <p>1,240 Student Tutored</p>
+                            <button className='btn btn-outline-success'>View More</button>
+                        </div>
+                    </div>
+                    <div className='col-lg-3 mr-5 shadow border border-black p-3 w-25 mx-auto text-center  mb-5 rounded'>
+                        <div className='mb-3'>
+                            <img src={teach} alt='iod' className='w-50' style={{ borderRadius: '20%' }} />
+                        </div>
+                        <div>
+                            <p className='fw-bold'>Professor Adebite Hope</p>
+                            <p>Physics and Mathematics</p>
+                            <p>1,240 Student Tutored</p>
+                            <button className='btn btn-outline-success'>View More</button>
                         </div>
 
                     </div>
-    </div>
-  )
+                    <div className='col-lg-3 mr-5 shadow border border-black p-3 w-25 mx-auto text-center  mb-5 rounded'>
+                        <div className='mb-3'>
+                            <img src={teach} alt='iod' className='w-50' style={{ borderRadius: '20%' }} />
+                        </div>
+                        <div>
+                            <p className='fw-bold'>Professor Adebite Hope</p>
+                            <p>Physics and Mathematics</p>
+                            <p>1,240 Student Tutored</p>
+                            <button className='btn btn-outline-success'>View More</button>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='row '>
+                    <div className='col-lg-3 mr-5 shadow border border-black p-3 w-25 mx-auto text-center  mb-5 rounded'>
+                        <div className='mb-3'>
+                            <img src={teach} alt='iod' className='w-50' style={{ borderRadius: '20%' }} />
+                        </div>
+                        <div>
+                            <p className='fw-bold'>Professor Adebite Hope</p>
+                            <p>Physics and Mathematics</p>
+                            <p>1,240 Student Tutored</p>
+                            <button className='btn btn-outline-success'>View More</button>
+                        </div>
+                    </div>
+                    <div className='col-lg-3 mr-5 shadow border border-black p-3 w-25 mx-auto text-center  mb-5 rounded'>
+                        <div className='mb-3'>
+                            <img src={teach} alt='iod' className='w-50' style={{ borderRadius: '20%' }} />
+                        </div>
+                        <div>
+                            <p className='fw-bold'>Professor Adebite Hope</p>
+                            <p>Physics and Mathematics</p>
+                            <p>1,240 Student Tutored</p>
+                            <button className='btn btn-outline-success'>View More</button>
+                        </div>
+
+                    </div>
+                    <div className='col-lg-3 mr-5 shadow border border-black p-3 w-25 mx-auto text-center  mb-5 rounded'>
+                        <div className='mb-3'>
+                            <img src={teach} alt='iod' className='w-50' style={{ borderRadius: '20%' }} />
+                        </div>
+                        <div>
+                            <p className='fw-bold'>Professor Adebite Hope</p>
+                            <p>Physics and Mathematics</p>
+                            <p>1,240 Student Tutored</p>
+                            <button className='btn btn-outline-success'>View More</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default AllLecturer
