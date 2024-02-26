703e97c850072ccd02283155c2858c37076f719e
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-26 19:52:21 +0100

    wip on the design

--- src/Teacher/Document.jsx
+++ src/Teacher/Document.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import TutorNavBar from './TeacherNavBar'
+import style from '../styles/LandNavBar.module.css'
 import Footer from '../Landing/Footer'
 
 const Document = () => {
@@ -22,6 +23,7 @@ const Document = () => {
                 </div>
 
                 <div className='container'>
+                    <div className='row'>
                     <div className="col-lg-6 mb-lg-0 mb-4">
                         <form method="post" action="">
                             <div className="row">
@@ -33,15 +35,22 @@ const Document = () => {
                                 </div>
                                 <div className="col-lg-12">
                                     <div className="text-center mt-4">
-                                        <button type="submit"  name="submit"  className="btn btn-main-1 w-100 text-medium rounded-0 py-3 px-4" >
-                                            
+                                        <button type="submit" name="submit" className="btn btn-main-1 w-100 text-medium text-light rounded-0 py-3 px-4" style={{ backgroundColor: '#FF9500' }} >
+                                            Submit
                                         </button>
                                     </div>
                                 </div>
                             </div>
                         </form>
                     </div>
+                    <div className="col-lg-6 mb-lg-0 mb-4">
+                        <div className=' mx-auto text-center'>
+                            <div style={{ height: '300px' }} className="overflow-auto bg-light p-lg-4 p-3 rounded">
+                            </div>
 
+                        </div>
+                    </div>
+</div>
                 </div>
 
 
