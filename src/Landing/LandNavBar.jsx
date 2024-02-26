9dfdb5d6f591593184a2b1397957d24a0e0c755c
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-26 12:26:38 +0100

    wip on the design

--- src/Landing/LandNavBar.jsx
+++ src/Landing/LandNavBar.jsx
@@ -8,49 +8,54 @@ const LandNavBar = () => {
 
   return (
     <>
-    <div className={style.body}>
+      <div className={style.body}>
         <div className={`${style.container} container mt-3 mx-auto p-2 text-center`}>
-                <p>Learning Today . . .  Leading Tomorrow</p>
+          <p>Learning Today . . .  Leading Tomorrow</p>
         </div>
         <div className='container'>
 
-        <nav className="navbar navbar-expand-sm navbar-light " id="home" >
+          <nav className="navbar navbar-expand-sm navbar-light " id="home" >
 
-    <a href="" className="navbar-brand p-3">
-        <img src={logo} alt='jjkbd' className='w-50' />
-    </a>
-      <button className="navbar-toggler" data-toggle="collapse" type="button" data-target="#sab">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-  <div className="collapse navbar-collapse" id="sab">    
-    <ul className="navbar-nav list-line mx-auto">               
-        <li className="nav-item active list-line-item mr-5 ">
-          <Link to="/" className="nav-link active text-center ">Home</Link>
-        </li>
-        <li className="nav-item dropdown list-line-item mr-5 " >
-          <Link to="/about" className="nav-link active text-center">About Us </Link>
-        </li>
-        
-        <li className="nav-item dropdown list-line-item mr-5 " >
-          <Link to="/contact" className='nav-link active text-center'>
-          Contact
-          </Link>
-        </li>
-      </ul>
-    <ul className="navbar-nav list-line ml-auto">               
+            <a href="" className="navbar-brand p-3">
+              <img src={logo} alt='jjkbd' className='w-50' />
+            </a>
+            <button className="navbar-toggler" data-toggle="collapse" type="button" data-target="#sab">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="sab">
+              <ul className="navbar-nav list-line mx-auto">
+                <li className="nav-item active list-line-item mr-5 ">
+                  <Link to="/" className="nav-link active text-center ">Home</Link>
+                </li>
+                <li className="nav-item dropdown list-line-item mr-5 " >
+                  <Link to="/about" className="nav-link active text-center">About Us </Link>
+                </li>
+
+                <li className="nav-item dropdown list-line-item mr-5 " >
+                  <Link to="/contact" className='nav-link active text-center'>
+                    Contact
+                  </Link>
+                </li>
+                <li className="nav-item dropdown list-line-item mr-5 " >
+                  <Link to="/contact" className='nav-link active text-center'>
+                    Tutor
+                  </Link>
+                </li>
+              </ul>
+              {/* <ul className="navbar-nav list-line ml-auto">               
         <li className="nav-item active list-line-item mr-5 ">
           <Link className="nav-link active text-center" to="/register">Register</Link>
-        </li>
-        <li className="nav-item active list-line-item mr-5 " >
+        </li> */}
+              {/* <li className="nav-item active list-line-item mr-5 " >
           <Link to="/login" className= 'text-light nav-link text-center btn btn' style={{backgroundColor:'#FF9500'}} >Login</Link>
-        </li>
-      </ul>
-      
-    </div>
-   </nav>
-   </div>
-    </div>
-</>
+        </li> */}
+              {/* </ul> */}
+
+            </div>
+          </nav>
+        </div>
+      </div>
+    </>
   )
 }
 
--- src/Landing/Lecturer-Profile.jsx
+++ src/Landing/Lecturer-Profile.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import LandNavBar from './LandNavBar'
 import teach from '../images/lectu.jpg'
 import aderonke from '../videos/aderonke.mp4'
+import Footer from './Footer'
 
 
 const Profile = () => {
@@ -12,7 +13,7 @@ const Profile = () => {
 
             <div className='container mt-5'>
                 <div className='row'>
-                    <div className='col-lg-4'>
+                    <div className='col-lg-4 text-center mx-auto'>
                         <img src={teach} alt='iod' className='w-50 rounded' />
                         <p><i className='fa fa-user'></i>Professor John Okafor</p>
                         <p><i className='fa fa-message'></i>JohnOkafor@gmail.com</p>
@@ -23,8 +24,8 @@ const Profile = () => {
                         <div className="card">
                             <div className="card-body mt-3">
                                 <div className="text-center">
-                                    <button id="bttn" className="mr-3 fs-5 btn btn"> <i className="fa fa-flag text-light"></i></button>
-                                    <button id="bttn" className="mr-3 fs-5 btn btn text-light"> Availability</button>
+                                    <button id="bttn" className=" fs-5 btn btn"> <i className="fa fa-flag" style={{color:'#FF9500'}} ></i></button>
+                                    <button id="bttn" className=" fs-5 btn btn text-dark"> Availability</button>
                                 </div><br />
                                 <div>
                                     <nav>
@@ -33,16 +34,7 @@ const Profile = () => {
                                                 <a className="nav-link active" style={{ color: '#A10035' }} href="#about">About</a>
                                             </li>
                                             <li className="nav-item">
-                                                <a className="nav-link" style={{ color: 'grey' }} href="#photos">Photos</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="nav-link" style={{ color: 'grey' }} href="#videos">Videos</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="nav-link" style={{ color: 'grey' }} href="#song">Song List</a>
-                                            </li>
-                                            <li className="nav-item">
-                                                <a className="nav-link" style={{ color: 'grey' }} href="#genre">Genre</a>
+                                                <a className="nav-link" style={{ color: 'grey' }} href="#Document">Document</a>
                                             </li>
                                         </ul>
                                     </nav>
@@ -79,16 +71,16 @@ const Profile = () => {
                             </div>
                             <hr/>
                             <div>
-                                <h4 id="videos" className="title">Videos</h4>
-                                <div class="row">
-                                    <div class="col-lg-4 mb-4 mb-lg-0">
-                                        <video src={aderonke} controls class="w-100 h-50 shadow-1-strong rounded mb-4" alt="Boat on Calm Water"></video>
+                                <h4 id="videos">Videos</h4>
+                                <div className="row">
+                                    <div className="col-lg-4 ">
+                                        <video src={aderonke} controls className="w-100 h-50 shadow-1-strong rounded " alt="Boat on Calm Water"></video>
                                     </div>
-                                    <div class="col-lg-4 mb-4 mb-lg-0">
-                                        <video src={aderonke} controls class="w-100 h-50 shadow-1-strong rounded mb-4" alt="Boat on Calm Water"></video>
+                                    <div className="col-lg-4 ">
+                                        <video src={aderonke} controls className="w-100 h-50 shadow-1-strong rounded " alt="Boat on Calm Water"></video>
                                     </div>
-                                    <div class="col-lg-4 mb-4 mb-lg-0">
-                                        <video src={aderonke} controls class="w-100 h-50 shadow-1-strong rounded mb-4" alt="Boat on Calm Water"></video>
+                                    <div className="col-lg-4 ">
+                                        <video src={aderonke} controls className="w-100 h-50 shadow-1-strong rounded " alt="Boat on Calm Water"></video>
                                     </div>
                                 </div>
                             </div>
@@ -100,6 +92,7 @@ const Profile = () => {
                 </div>
 
             </div>
+            <Footer/>
         </div>
     )
 }
