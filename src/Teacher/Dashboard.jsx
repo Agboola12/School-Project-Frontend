3207eabd48cd33a609d12db983e680ec8dee1246
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-26 13:46:20 +0100

    wip on the design

--- src/Teacher/Dashboard.jsx
+++ src/Teacher/Dashboard.jsx
@@ -1,30 +1,34 @@
 import React from 'react'
 import TutorNavBar from './TeacherNavBar'
 import Footer from '../Landing/Footer'
+import style from '../styles/LandNavBar.module.css'
+
 
 const Dashboard = () => {
-  return (
-    <div>
-        <TutorNavBar/>
+    return (
+        <div>
+            <TutorNavBar />
 
-        <div className="main-content">
+            <div className="main-content">
 
                 <div className="about-sect">
                     <div className="container">
                         <div className="row">
                             <div className="col-lg-12">
-                                <div className="grid-item inner-box grid-cover mb-5" style={{ backgroundImage: `url(${Worshipp})` }}>
+                                <div className="grid-item inner-box grid-cover mb-5 " id={style.tutor} >
                                     <div className="overlay-hero"></div>
-                                    <h2 className="text-white z-index-1">Verification Code</h2>
+                                    <h2 className="text-white z-index "> Profile</h2>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-</div>
-<Footer/>
-    </div>
-  )
+            </div>
+
+
+            <Footer />
+        </div>
+    )
 }
 
 export default Dashboard
