c045876326f617a0d4688a4a0bef67b15b080158
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-26 17:39:22 +0100

    wip on the design

--- src/Teacher/Document.jsx
+++ src/Teacher/Document.jsx
@@ -3,13 +3,11 @@ import TutorNavBar from './TeacherNavBar'
 import Footer from '../Landing/Footer'
 
 const Document = () => {
-  return (
-    <div>
-                <div>
+    return (
+        <div>
             <TutorNavBar />
 
             <div className="main-content">
-
                 <div className="about-sect">
                     <div className="container">
                         <div className="row">
@@ -22,11 +20,36 @@ const Document = () => {
                         </div>
                     </div>
                 </div>
-    </div>
-</div>
-<Footer/>
-    </div>
-  )
+
+                <div className='container'>
+                    <div className="col-lg-6 mb-lg-0 mb-4">
+                        <form method="post" action="">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                    <div className="form-floating mb-3">
+                                        <input type='text' required className='form-control' />
+                                        <label>Verification Code</label>
+                                    </div>
+                                </div>
+                                <div className="col-lg-12">
+                                    <div className="text-center mt-4">
+                                        <button type="submit"  name="submit"  className="btn btn-main-1 w-100 text-medium rounded-0 py-3 px-4" >
+                                            
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+
+
+
+            </div>
+            <Footer />
+        </div>
+    )
 }
 
 export default Document
