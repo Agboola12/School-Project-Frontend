b5733facd65e216e272dcbfe426655978e09f048
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-28 12:31:02 +0100

    wip on the login

--- src/Teacher/Dashboard.jsx
+++ src/Teacher/Dashboard.jsx
@@ -3,13 +3,44 @@ import TutorNavBar from './TeacherNavBar'
 import Footer from '../Landing/Footer'
 import style from '../styles/LandNavBar.module.css'
 import avatar from '../images/avatar.jpeg'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
 
 const Dashboard = () => {
+    const dispatch = useDispatch();
     const { loginAdmin: user } = useSelector((state) => state.admin);
 
-    console.log(user);
+    const userImageUrl = useRef()
+
+
+    const handleImage = () => {
+        const data = new FormData();
+        data.append("userImageUrl", userImageUrl.current.files[0])
+
+        axios.patch(BaseUrl + `userImage/${user._id}`, data)
+            .then((res) => {
+                if (res.data.status) {
+                    axios.get(BaseUrl + "getUser").then(res => {
+                        if (res.data.status) {
+                            dispatch(setuser(res.data.data));
+                            // const formattedDate = moment(res.data.joinDate).format('YYYY-MM-DD');
+                        }
+                    })
+                }
+                const result = res.data.message;
+                toast.success(result, {
+                    autoClose: 3000,
+                })
+            })
+            .catch((err) => {
+                const error = err.response?.data?.message || 'An error occurred';
+                toast.error(error, {
+                    autoClose: 3000,
+                });
+                console.error(err.message);
+            })
+    }
 
     return (
         <div>
@@ -23,7 +54,6 @@ const Dashboard = () => {
                             <div className="col-lg-12">
                                 <div className="grid-item inner-box grid-cover mb-5 " id={style.tutor} >
                                     <div className="overlay-hero"></div>
-                                    {/* <h2 className="text-white z-index "> Profile</h2> */}
                                 </div>
                             </div>
                         </div>
@@ -32,11 +62,11 @@ const Dashboard = () => {
 
 
                 <div className="info-section pb-5">
-            <div className="container">
-            <h2 className="text-dark text-center mt-1 fw-bold fs-1"> Profile</h2>
-                <div className="col-lg-12">
+                    <div className="container">
+                        <h2 className="text-dark text-center mt-1 fw-bold fs-1"> Profile</h2>
+                        <div className="col-lg-12">
 
-                    <div className="mb-4">
+                            {/* <div className="mb-4">
                         <label htmlFor="image">
                             <input type="file" id="image" name="image" className="d-none" />
                             <div className="avatar-circle cursor-pointer rounded-circle mx-auto mb-2">
@@ -44,71 +74,51 @@ const Dashboard = () => {
                             </div>
                             <em>Change your photo</em>
                         </label>
-                    </div>
-                    
-                    <div className="d-flex justify-content-between align-items-center mb-4">
-                        <h3 className="mb-0">Profile</h3>
-                        <a href="#" className="text-dark">Edit Profile</a>
-                    </div>
+                    </div> */}
 
-                    <form>
-                        <div className="row">
-                            <div className="col-lg-4">
-                                <div className="form-floating mb-3">
-                                    <input type="text" className="form-control" placeholder="Mr" value="Mr" disabled /> 
-                                    <label>Title</label>
+                            <div className="mb-4">
+                                <label for="image">
+                                    <input type="file" onChange={handleImage} ref={userImageUrl} className="" />
+                                    <div className="avatar-circle cursor-pointer rounded-circle mx-auto mb-2">
+                                        <img src={avatar} alt='sjnd' className="img-fluid object-fit-cover object-position-center w-100 h-100" />
+                                    </div>
+                                    <em>Change your photo </em>
+                                </label>
+                            </div>
+
+                            <div className="d-flex justify-content-between align-items-center mb-4">
+                                <h3 className="mb-0">Profile</h3>
+                                <Link to="/editProfile" className="text-dark">Edit Profile</Link>
+                            </div>
+
+                            <form>
+                                <div className="row">
+                                    <div className="col-lg-4">
+                                        <div className="form-floating mb-3">
+                                            <input type="text" className="form-control" placeholder="Full name" value={user?.fullName} disabled />
+                                            <label>Full name</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <div className="form-floating mb-3">
+                                            <input type="email" className="form-control" placeholder="Email address" value={user?.email} disabled />
+                                            <label>Email address</label>
+                                        </div>
+                                    </div>
+                                    <div className="col-lg-4">
+                                        <div className="form-floating mb-3">
+                                            <input type="text" className="form-control" placeholder="Phone number" value={user?.department} disabled />
+                                            <label>Department</label>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col-lg-4">
-                                <div className="form-floating mb-3">
-                                    <input type="text" className="form-control" placeholder="Full name" value="Mariam Danjuman" disabled /> 
-                                    <label>Full name</label>
-                                </div>
-                            </div>
-                            <div className="col-lg-4">
-                                <div className="form-floating mb-3">
-                                    <input type="email" className="form-control" placeholder="Email address" value="mariam@hello.com" disabled />
-                                    <label>Email address</label>
-                                </div>
-                            </div>
-                            <div className="col-lg-4">
-                                <div className="form-floating mb-3">
-                                    <input type="text" className="form-control" placeholder="Phone number" disabled value="9484739202" />
-                                    <label>Phone number</label>
-                                </div>
-                            </div>
-                             <div className="col-lg-4">
-                                <div className="form-floating mb-3">
-                                    <input type="text" value="+444" className="form-control" placeholder="Postal code" disabled />
-                                    <label>Postal code</label>
-                                </div>
-                            </div>
-                            <div className="col-lg-4">
-                                <div className="form-floating mb-3">
-                                    <input type="text" value="45, liverpool street, UK" className="form-control" placeholder="Address" disabled />
-                                    <label>Address</label>
-                                </div>
-                            </div>
-                            <div className="col-lg-4 mb-3">
-                                <p className="mb-0"><label>Date of Birth</label></p>
-                                <span>20/07/1980</span>
-                            </div>
-                            <div className="col-lg-4 mb-3">
-                                <p className="mb-0"><label>Gender</label></p>
-                                <span>Male</span>
-                            </div>
-                             <div className="col-lg-4 mb-3">
-                                <p className="mb-0"><label>Status</label></p>
-                                <span className="badge text-bg-success">Member</span>
-                            </div>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
-            </div>
-        </div>
 
 
-                
+
             </div>
 
 
