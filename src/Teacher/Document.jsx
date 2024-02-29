c4eea3c67b4d767ee67a32f219203c39d20030c0
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-29 09:06:37 +0100

    wip on the uploading the document

--- src/Teacher/Document.jsx
+++ src/Teacher/Document.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import TutorNavBar from './TeacherNavBar'
 import style from '../styles/LandNavBar.module.css'
 import Footer from '../Landing/Footer'
 
 const Document = () => {
+    const title = useRef();
+    const youtubeLink = useRef();
+    const pdfFile = useRef();
+    const pdfLink = useRef();
+    const [Error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false)
+
+
+
+
     return (
         <div>
             <TutorNavBar />
@@ -23,29 +33,43 @@ const Document = () => {
                 </div>
 
                 <div className='container mb-5'>
-                <h2 className="text-dark text-center mt-4 fw-bold "> Uploading Of Document</h2>
+                    <h2 className="text-dark text-center mt-4 fw-bold "> Uploading Of Document</h2>
                     <div className='row mt-5'>
                         <div className="col-lg-6 mb-lg-0 mb-5">
+                            <p>
+                                <b className="text-danger">{Error}</b>
+                            </p>
+                            <div
+                                className={`alert alert-${result.status ? 'success' : 'danger'} text-center text-${result.status ? 'success' : 'danger'
+                                    } d-${result.message ? 'block' : 'none'}`}>
+                                {result.message}
+                            </div>
                             <form method="post" action="">
                                 <div className="row">
                                     <div className="col-lg-12">
                                         <div className="form-floating mb-3">
-                                            <input name='title' type='text' className="form-control"  placeholder="Sermon Title" />
+                                            <input name='title' type='text' className="form-control" ref={title} placeholder=" Title" />
                                             <label>Title</label>
                                         </div>
                                         <div className="form-floating mb-3">
-                                            <input name='youtube' type='text' className="form-control" placeholder="Youtube" />
+                                            <input name='youtube' type='text' className="form-control" ref={youtubeLink} placeholder="Youtube" />
                                             <label>YouTube Link</label>
                                         </div>
                                         <div className="form-floating mb-3">
-                                            <input name='youtube' type='text' className="form-control" placeholder="Pdf Link" />
+                                            <input name='youtube' type='text' className="form-control" ref={pdfLink} placeholder="Pdf Link" />
+                                            <label>Pdf Link</label>
+                                        </div>
+                                        <div className="form-floating mb-3">
+                                            <input type="file" ref={pdfFile} onChange={handleChange} />
                                             <label>Pdf Link</label>
                                         </div>
                                     </div>
                                     <div className="col-lg-12">
                                         <div className="text-center mt-4">
-                                            <button type="submit" name="submit" className="btn btn-main-1 w-100 text-medium text-light rounded-0 py-3 px-4" style={{ backgroundColor: '#FF9500' }} >
-                                                Submit
+                                            <button type="submit" disabled={isLoading} name="submit" className="btn btn-main-1 w-100 text-medium text-light rounded-0 py-3 px-4" style={{ backgroundColor: '#FF9500' }} >
+                                                <b>
+                                                    {isLoading ? "Loading..." : "Submit"}
+                                                </b>
                                             </button>
                                         </div>
                                     </div>
