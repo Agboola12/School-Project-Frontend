a490b9eeda198109e6cdc23a7be86bf0c8ecc63d
Agbooa12 <agboolaelijah077@gmail.com>
2024-02-29 13:19:18 +0100

    wip on the uploading the document

--- src/Teacher/Document.jsx
+++ src/Teacher/Document.jsx
@@ -6,6 +6,7 @@ import axios from 'axios'
 import BaseUrl from '../BaseUrl'
 
 const Document = () => {
+
     const title = useRef();
     const youtubeLink = useRef();
     const pdfLink = useRef();
@@ -13,6 +14,18 @@ const Document = () => {
     const [Error, setError] = useState("");
     const [isLoading, setIsLoading] = useState(false)
     const [info, setInfo] = useState([])
+    const [editID, setEditID] = useState('')
+    const [file, setFile] = useState(null);
+
+    const [user, setUser] = useState({});
+
+
+    const inputIsChanging = (e) => {
+        const name = e.target.name
+        const value = e.target.value
+        console.log(value)
+        setUser({ ...user, [name]: value })
+    }
 
     const [result, setResult] = useState({
         message: "",
@@ -25,7 +38,7 @@ const Document = () => {
     const FetchData = () => {
         axios.get(BaseUrl + 'getDocument')
             .then(res => {
-            console.log(res.data.data);
+                console.log(res.data.data);
                 setInfo(res.data.data);
             })
             .catch(err => {
@@ -33,10 +46,9 @@ const Document = () => {
             })
     }
 
-    const [file, setFile] = useState(null);
 
     const handleFileChange = (e) => {
-      setFile(e.target.files[0]);
+        setFile(e.target.files[0]);
     };
     // console.log();
 
@@ -47,6 +59,41 @@ const Document = () => {
         data.append("pdfLink", pdfLink.current.value)
         data.append("pdfFile", file);
 
+        setIsLoading(true);
+        if(editID){
+            axios.patch(BaseUrl +`EditInfo/${editID}`,data).then((res)=>{
+                if(res.data.status){
+                    FetchData();
+                        // setUser({
+                    //         title: "",
+                    //         message: "",
+                    //     });
+                    // userImageUrl.current.value = '';
+                    setEditID('')
+                }
+                else {
+                    setError(res.data.message);
+                }
+                setResult({
+                    message: res.data.message,
+                    status: res.data.status
+                })
+                setTimeout(() => {
+                    setResult({
+                        message: "",
+                        status: false
+                    });
+                    setError("");
+                    resetForm();
+                }, 3000);
+            }).catch((err) => {
+                console.log(err.message);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+        }
+        else{
         setIsLoading(true);
         axios.post(BaseUrl + "tutorInfo", data).then(
             res => {
@@ -75,6 +122,7 @@ const Document = () => {
             .finally(() => {
                 setIsLoading(false);
             });
+        }
     }
 
     const DeleteInfo = (_id) => {
@@ -100,14 +148,17 @@ const Document = () => {
             .catch((err) => {
                 console.log(err.message);
             })
-        };
+    };
 
-        const EditInfo = (_id) => {
 
-            // setEditID(_id)
-            // setUser(()=>news.find((val)=>val._id==_id))
-            
-        }
+    const EditInfo = (_id) => {
+        setEditID(_id)
+        setUser(() => info.find((val) => val._id == _id))
+
+    }
+
+
+
 
     return (
         <div>
@@ -130,33 +181,33 @@ const Document = () => {
                 <div className='container mb-5'>
                     <h2 className="text-dark text-center mt-4 fw-bold "> Uploading Of Document</h2>
                     <div className='row mt-5'>
+                        <p>
+                            <b className="text-danger">{Error}</b>
+                        </p>
+                        <div
+                            className={`alert alert-${result.status ? 'success' : 'danger'} text-center text-${result.status ? 'success' : 'danger'
+                                } d-${result.message ? 'block' : 'none'}`}>
+                            {result.message}
+                        </div>
                         <div className="col-lg-6 mb-lg-0 mb-5">
-                            <p>
-                                <b className="text-danger">{Error}</b>
-                            </p>
-                            <div
-                                className={`alert alert-${result.status ? 'success' : 'danger'} text-center text-${result.status ? 'success' : 'danger'
-                                    } d-${result.message ? 'block' : 'none'}`}>
-                                {result.message}
-                            </div>
                             <form method="post" action="">
                                 <div className="row">
                                     <div className="col-lg-12">
                                         <div className="form-floating mb-3">
-                                            <input name='title' type='text' className="form-control" ref={title} placeholder=" Title" />
+                                            <input name='title' type='text' className="form-control" onChange={inputIsChanging} value={user?.title} ref={title} placeholder=" Title" />
                                             <label>Title</label>
                                         </div>
                                         <div className="form-floating mb-3">
-                                            <input name='youtube' type='text' className="form-control" ref={youtubeLink} placeholder="Youtube" />
+                                            <input name='youtubeLink' type='text' className="form-control" onChange={inputIsChanging} value={user?.youtubeLink} ref={youtubeLink} placeholder="Youtube" />
                                             <label>YouTube Link</label>
                                         </div>
                                         <div className="form-floating mb-3">
-                                            <input name='youtube' type='text' className="form-control" ref={pdfLink} placeholder="Pdf Link" />
+                                            <input name='pdfLink' type='text' className="form-control" onChange={inputIsChanging} value={user?.pdfLink} ref={pdfLink} placeholder="Pdf Link" />
                                             <label>Pdf Link</label>
                                         </div>
                                         <div className="form-floating mb-3">
                                             <label className=''> Document</label>
-                                            <input id="file" type="file" ref={pdfFile} onChange={handleFileChange } accept=".pdf,.doc,.docx .txt"  />
+                                            <input id="file" type="file" ref={pdfFile} onChange={handleFileChange} accept=".pdf,.doc,.docx .txt" />
                                         </div>
                                     </div>
                                     <div className="col-lg-12">
@@ -174,23 +225,27 @@ const Document = () => {
                         <div className="col-lg-6 mb-lg-0 mb-5">
                             <div className=' mx-auto text-center'>
                                 <div style={{ height: '300px' }} className="overflow-auto bg-light p-lg-4 p-3 rounded">
-                                        <h5 className="text-center mb-5">Document</h5>
-                                        <hr />
-            {info.sort((a,b)=>b.createdAt.localeCompare(a.createdAt)).map((post) => (
-                                            <div className="mb-3 border-bottom">
-                                                <h6>{post.title}</h6>
-                                                <p className="text-muted text-medium ft-sm"><em>Published on {post.youtubeLink}.</em></p>
-                                                <p className="text-muted text-medium ft-sm"><em>Published on {post.pdfLink}.</em></p>
-                                                <p className="text-muted text-medium ft-sm"><em>Published on {post.pdfFile}.</em></p>
-                                                <button type="submit"  name="submit" onClick={() => DeleteInfo(post._id)} className="btn btn text-danger" >
-                                                     <i className="fa fa-trash"></i>
-                                                </button>
-                                                <button type="submit" name="submit" onClick={() =>EditInfo(post._id)} className="btn btn text-success  mr-2" >
+                                    <h5 className="text-center mb-5">Document</h5>
+                                    <hr />
+                                    {info.sort((a, b) => b.createdAt.localeCompare(a.createdAt)).map((post) => (
+                                        <div className="mb-3 border-bottom">
+                                            <h6>{post.title}</h6>
+                                            <p className="text-muted text-medium ft-sm"><em>Youtube Link: {post.youtubeLink}.</em></p>
+                                            <p className="text-muted text-medium ft-sm"><em>Document Link: {post.pdfLink}.</em></p>
+
+                                            {/* <iframe src={post.pdfFile} width="800" height="600" title="Document" className='bg-danger' /> */}
+                                            <a href={post.pdfFile} download>Download Document</a>
+
+                                            <p className="text-muted text-medium ft-sm"><em>Document File: {post.pdfFile}.</em></p>
+                                            <button type="submit" name="submit" onClick={() => DeleteInfo(post._id)} className="btn btn text-danger" >
+                                                <i className="fa fa-trash"></i>
+                                            </button>
+                                            <button type="submit" name="submit" onClick={() => EditInfo(post._id)} className="btn btn text-success  mr-2" >
                                                 <i className='fa fa-edit'></i>
                                             </button>
-                                            </div>
+                                        </div>
 
-                                        ))}
+                                    ))}
                                 </div>
 
                             </div>
