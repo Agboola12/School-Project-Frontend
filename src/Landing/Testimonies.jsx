4595a24ce91df1188901bd238b291ebc5b86bf74
Agbooa12 <agboolaelijah077@gmail.com>
2024-03-01 15:34:23 +0100

    wip on functionality

--- src/Landing/Testimonies.jsx
+++ src/Landing/Testimonies.jsx
@@ -1,120 +1,94 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import LandNavBar from '../Landing/LandNavBar'
 import Footer from '../Landing/Footer'
-import { FaFacebook } from "react-icons/fa";
-import { FaTwitter } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa6";
+import style from '../styles/LandNavBar.module.css'
 
 
 const Testimonies = () => {
 
+    const fullName = useRef();
+    const message = useRef();
+    const [error, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false)
+    const [result, setResult] = useState({
+        message: "",
+        status: false
+    })
+
+    const HandleSubmit = (e, resetForm)=>{
+        e.preventDefault(e);
+
+        const info = {
+            fullName: fullName.current.value,
+            message: message.current.value,
+        };
+
+        setIsLoading(true);
+
+        axios.post(BaseUrl + 'schoolTestimony', info )
+        .then((res)=>{
+            if(res.data.status){
+                setResult({
+                    message: res.data.message,
+                    status: res.data.status,
+                });
+            }
+            setTimeout(() => {
+                setResult({
+                    message: "",
+                    status: false,
+                });
+                setError("");
+                resetForm();
+            }, 3000);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+        .finally(() => {
+            setIsLoading(false);
+        });
+    }
 
 
   return (
     <div>
       <LandNavBar />
-      <div className='bg-light'>
+      <div className='bg-light p-5'>
+                <div className="container p-4">
+                    <div className="row justify-content-center">
+                        <div className="col-lg-8 ">
+                            <div className=" p-4 mb-5 bg-white p-5">
+                                <h2 className="text-center fw-bold">Testimony</h2>
+                                <p>
+                                    <b className="text-danger">{error}</b>
+                                </p>
+                                <div
+                                    className={`alert alert-${result.status ? 'success' : 'danger'} text-center text-${result.status ? 'success' : 'danger'
+                                        } d-${result.message ? 'block' : 'none'}`}>
+                                    {result.message}
+                                </div>
+                                <form action="" method="post" enctype="multipart/form-data" onSubmit={HandleSubmit} className='mt-5'>
+                                    <div className="form-group">
+                                        <input type="text" ref={fullName} className="form-control  d-grid col-12" required placeholder="Enter your FullName" />
+                                    </div>
+                                    <div className="form-group">
+                                        <textarea type="message" ref={message} className="form-control  d-grid col-12" required placeholder="Enter your Messgae" />
+                                    </div>
+                                    <div className="form-check mt-5">
+                                        <button disabled={isLoading} name="submit" className={`${style.container} btn btn-warning h-75 p-2 fw-bold fs-4 d-grid gap-2 col-12 mx-auto`}>
+                                            <b>
+                                                {isLoading ? "Loading..." : "Submit"}
+                                            </b>
+                                        </button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
 
-
-        <div className="container  p-5">
-          <div className="row">
-            <div className='col-lg-6 text-left'>
-              <p className='h2'> Contact Us </p>
+                    </div>
+                </div>
             </div>
-            <div className='col-lg-6 text-right'>
-              <p>
-                Welcome to SkillBridge's Pricing Plan page, where we offer two comprehensive options to cater to your needs:
-                Free and Pro. We believe in providing flexible and affordable pricing options for our services. Whether
-                you're an individual looking to enhance your skills or a business seeking professional development solutions,
-                we have a plan that suits you. Explore our pricing options below and choose the one that best fits your requirements.
-              </p>
-            </div>
-          </div>
-
-          {/* section 2  form*/}
-
-          <div className="row bg-white p-5 mt-5">
-            <div className="col-lg-8 p-3">
-
-              <form>
-                <div className="row mt-3">
-                  <div className="col-lg-6">
-                    <label htmlFor="">First Name</label>
-                    <input type="text" className="form-control" placeholder="Enter Firstname" name="email" />
-                  </div>
-
-                  <div className="col-lg-6">
-                    <label htmlFor="">Last Name</label>
-                    <input type="text" className="form-control" placeholder="Enter Lastname" name="pswd" />
-                  </div>
-                </div>
-
-                <div className="row mt-3">
-
-                  <div className="col-lg-6 ">
-                    <label htmlFor="">Email</label>
-                    <input type="text" className="form-control" placeholder="Enter Email" name="email" />
-                  </div>
-
-                  <div className="col-lg-6">
-                    <label htmlFor=""> Phone</label>
-                    <input type="text" className="form-control" placeholder="Enter Your Subject" name="email" />
-                  </div>
-                </div>
-                <div className='row mt-3'>
-                  <div className="col-lg-12">
-                    <label htmlFor="">Subject</label>
-                    <input type="text" className="form-control" placeholder="Enter Phone Number" name="pswd" />
-                  </div>
-                </div>
-
-                <div className="row mt-3">
-                  <div className="col-lg-12">
-                    <label htmlFor="">Message</label>
-                    <textarea name="Message" id="" className='form-control' placeholder='Enter Your Message'></textarea>
-                  </div>
-                </div>
-                <div className='row'>
-                  <div className='col-lg-12 text-center'>
-                <button className='btn btn fw-bold text-center text-white p-3 mt-5' style={{ backgroundColor: '#FF9500' }}>Send Your Message</button>
-                  </div>
-                </div>
-              </form>
-
-            </div>
-            <div className="col-lg-4">
-              <div className='bg-light p-3 text-center rounded-lg'> 
-                <i className=' text-center mx-auto fa fa-envelope'></i>
-                <p>support@skillbridge</p>
-              </div>
-
-              <div className='bg-light p-3 mt-3 text-center rounded-sm'> 
-                <i className='fa fa-phone'></i>              
-                <p>+91 00000 00000</p>
-              </div>
-
-              <div className='bg-light p-3 mt-3 text-center rounded-sm '>
-                <i className='fa fa-location-dot'></i>
-                <p>Some Where in the World</p>
-              </div>
-
-              <div className='bg-light mt-3 p-3 mx-auto text-center'>
-                <i className='p-2 text-center mx-auto'><FaFacebook /></i>
-                <i className='p-2 text-center mx-auto'><FaTwitter /></i>
-                <i className='p-2 text-center mx-auto'><FaLinkedin /></i> <br/>
-                   
-                <p>Social Profiles</p>
-              </div>
-
-            </div>
-          </div>
-        </div>
-
-
-
-
-
-      </div>
 
       <Footer />
     </div>
